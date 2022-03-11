a9e4e5a3d51a7c58ce47e6da40f360f3afa2522f
Fernando de Souza Saraiva - PrestServ <fernando.saraiva.prestserv@petrobras.com.br>
2022-03-11 14:51:44 -0300

    Criando o insert

--- src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -3,11 +3,20 @@ package pos_java_jdbc.pos_java_jdbc;
 import org.junit.Test;
 
 import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
 
 public class TesteBancoJdbc {
 
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(6L);
+		userposjava.setNome("Paulo Ricardo");
+		userposjava.setEmail("pauloricardo@outlook.com.br");
+		
+		userPosDAO.salvar(userposjava);
 	}
 }
